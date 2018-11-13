d23687c82cf5e92140b3719b8c29d19678c09f7f
Julian Andres <julianbolanos79@gmail.com>
2018-11-13 14:40:46 -0500

    ............´

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -9,7 +9,15 @@ class Conexion
         // $link = new PDO("mysql:host=localhost;dbname=proyectofinal",
         //     "root",
         //     "");
-        $link = new PDO('pgsql:user=postgres dbname=proyectofinal password=123');
+        // $link = new PDO('pgsql:user=postgres dbname=proyectofinal password=123');
+
+
+        // $link->exec("set names utf8");
+
+        // return $link;
+
+    	$link = new PDO('pgsql:user=jvdwioghpjqleb dbname=d42v3gmecvlgdd  password=ecb8e26902751ca156b5727322ab14f814d520244e2b6be875af2605cf6f4724 host=
+ec2-23-21-171-249.compute-1.amazonaws.com' );
 
 
         $link->exec("set names utf8");
