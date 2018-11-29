961d649b005cf28b9cd2df0901248ad5f5e03fa5
Julian Andres <julianbolanos79@gmail.com>
2018-11-29 16:35:17 -0500

    db

--- proyecto (2).sql
+++ proyecto (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-11-2018 a las 02:14:47
+-- Tiempo de generación: 29-11-2018 a las 22:34:19
 -- Versión del servidor: 10.1.34-MariaDB
 -- Versión de PHP: 7.2.7
 
@@ -63,6 +63,15 @@ CREATE TABLE `ambiente` (
   `ubicacionambiente` varchar(100) COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Volcado de datos para la tabla `ambiente`
+--
+
+INSERT INTO `ambiente` (`idambiente`, `idprograma`, `nombreambiente`, `ubicacionambiente`) VALUES
+(6, 5, 'AMBIENTE L', ''),
+(7, 6, 'AMBIENTE I1', 'CUALQUIER COSA'),
+(8, 4, 'AMBIENTE MULTIMEDIA', '');
+
 -- --------------------------------------------------------
 
 --
@@ -76,6 +85,21 @@ CREATE TABLE `aprendiz` (
   `telefonoaprendiz` double NOT NULL,
   `emailaprendiz` varchar(50) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `aprendiz`
+--
+
+INSERT INTO `aprendiz` (`numdocumentoaprendiz`, `numeroficha`, `nombreaprendiz`, `telefonoaprendiz`, `emailaprendiz`) VALUES
+(10, 1493990, 'Melany Alejandra Rojas Troyano ', 3265974231, 'sakdjhaskdjs@gmail.com'),
+(11, 1493990, ' Alejandra Rojas Troyano Lopez', 3265974232, 'sakdjhaskdjs@gmail.com'),
+(12, 1493990, 'Karen Molina Cobo ', 3265974233, 'sakdjhaskdjs@gmail.com'),
+(13, 1493990, 'Daniel Yordanier Valencia Troyano ', 3265974234, 'sakdjhaskdjs@gmail.com'),
+(15, 1493990, ' Danny Peña ', 3265974236, 'sakdjhaskdjs@gmail.com'),
+(16, 1493990, 'Mariana Bolaños', 3265974237, 'sakdjhaskdjs@gmail.com'),
+(17, 1493990, 'Tatiana Riascos', 3265974238, 'sakdjhaskdjs@gmail.com'),
+(18, 1493990, 'Melany Alejandra Rojas Troyano ', 3265974239, 'sakdjhaskdjs@gmail.com'),
+(141, 1493990, 'Nare Alejandro Manquillo Cobo', 3265974235, 'sakdjhaskdjs@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -102,8 +126,17 @@ CREATE TABLE `articulo` (
 --
 
 INSERT INTO `articulo` (`idarticulo`, `idambiente`, `idequipo`, `idcategoria`, `tipoarticulo`, `modeloarticulo`, `marcaarticulo`, `caracteristicaarticulo`, `estadoarticulo`, `numinventariosena`, `serialarticulo`) VALUES
-(1, NULL, NULL, NULL, 'SDFSDF', 'SDFASDF', 'SDFSDFSD', '', 'ACTIVO', 'sdfsdf', '123123'),
-(3, NULL, NULL, NULL, 'TECLADO', '54A6S', 'ACER', 'ASDASDAKJDHKAJDHAKSJ', 'ACTIVO', '123', '654sd');
+(4, 6, 1, 1, 'MOUSE', 'SFDS54', 'ACER', NULL, 'ACTIVO', '123Z', '7898x'),
+(5, 6, 1, 1, 'CPU', NULL, NULL, NULL, 'ACTIVO', '', ''),
+(6, 6, 1, 1, 'TECLADO', '54132', NULL, NULL, 'DAÑADO', '31212', ''),
+(7, 6, 1, 1, 'MONITOR', 'SDSDAS', 'HP', NULL, 'ACTIVO', '', 'asdsa'),
+(8, 7, 3, 1, 'MOUSE ', '32132', 'HP', NULL, 'ACTIVO', 'zxcz', 'zxcz'),
+(9, 7, 3, 1, 'CPU', 'ASDA', 'LENOVO', '...', 'DAÑADO', '1232', 'asd12'),
+(10, 7, 3, 1, 'TECLADO', 'ZXCZCS', 'XCXC', NULL, 'ACTIVO', '', '321w'),
+(11, 8, 2, 1, 'MONITOR', '2321', 'LG', NULL, 'ACTIVO', '', 'dasd2'),
+(12, 8, 2, 1, 'TECLADO', 'ASDSAD', 'ASDASD', NULL, 'ACTIVO', 'assad', 'asdas'),
+(13, 8, 2, 1, 'CPU', 'ADASDAS', NULL, NULL, 'ACTIVO', '324234', 'asdas'),
+(14, 7, 3, 1, 'MONITOR', 'ADSADA', 'SASA', '', 'ACTIVO', '', '');
 
 -- --------------------------------------------------------
 
@@ -130,6 +163,13 @@ CREATE TABLE `categoria` (
   `nombrecategoria` varchar(50) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`idcategoria`, `nombrecategoria`) VALUES
+(1, 'TECNOLOGíA ');
+
 -- --------------------------------------------------------
 
 --
@@ -145,6 +185,15 @@ CREATE TABLE `equipo` (
   `numarticulosagregados` varchar(50) COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Volcado de datos para la tabla `equipo`
+--
+
+INSERT INTO `equipo` (`idequipo`, `nombreequipo`, `estadoequipo`, `numarticulosequipo`, `observacionequipo`, `numarticulosagregados`) VALUES
+(1, 'A20', 'ACTIVADO', '4', '', '4'),
+(2, 'D01', 'ACTIVADO', '3', '', '3'),
+(3, 'I33', 'DESACTIVADO', '6', 'EL EQUIPO PRESENTA UN GOLPE EN LA ESQUINA SUPERIOR DERECHA DE LA PANTALLA', '4');
+
 -- --------------------------------------------------------
 
 --
@@ -159,6 +208,13 @@ CREATE TABLE `ficha` (
   `fechafin` varchar(50) COLLATE utf8_bin NOT NULL,
   `jornadaficha` varchar(50) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `ficha`
+--
+
+INSERT INTO `ficha` (`numeroficha`, `idprograma`, `idambiente`, `fechainicio`, `fechafin`, `jornadaficha`) VALUES
+(1493990, 5, 6, '11/11/2011', '11/11/2013', 'TARDE');
 
 -- --------------------------------------------------------
 
@@ -188,6 +244,15 @@ CREATE TABLE `programa` (
   `tipoprograma` varchar(50) COLLATE utf8_bin DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Volcado de datos para la tabla `programa`
+--
+
+INSERT INTO `programa` (`idprograma`, `nombreprograma`, `duracionprograma`, `tipoprograma`) VALUES
+(4, 'DESARROLLO WEB', '', 'TÉCNICO'),
+(5, 'ADSI', '', 'TECNÓLOGO'),
+(6, 'INGLES', '', 'COMPLEMENTARIO');
+
 -- --------------------------------------------------------
 
 --
@@ -208,7 +273,9 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`numdocumentousuario`, `idprograma`, `nombreusuario`, `contraseniausuario`, `rolusuario`, `fotousuario`) VALUES
-(123, NULL, 'ADMINISTRADOR', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 'ADMINISTRADOR', '');
+(123, NULL, 'ADMINISTRADOR', '3c9909afec25354d551dae21590bb26e38d53f2173b8d3dc3eee4c047e7ab1c1eb8b85103e3be7ba613b31bb5c9c36214dc9f14a42fd7a2fdb84856bca5c44c2', 'ADMINISTRADOR', ''),
+(777, 5, 'INSTRUCTOR ', '093763d6b1457e9fb44eeb346737783ad13bb739370d4a7d42d9cade8da9040b8c64e525aa0f2afde2dd5a98e763fbde69a679098603083c6a76fa2f66aa1188', 'INSTRUCTOR', 'vistas/img/usuarios/777/899.png'),
+(888, 6, 'ESPECIAL', 'bb602aa6ebb8decd4a7293b1c428cf4889df083d0984378ceefc600a371ac96de20ed1fbc8adf3baa8e63a28d20b750b1dd2512c51cf78490b602b5bc50e47c1', 'ESPECIAL', 'vistas/img/usuarios/888/852.png');
 
 --
 -- Índices para tablas volcadas
@@ -310,49 +377,49 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `actacompromiso`
 --
 ALTER TABLE `actacompromiso`
-  MODIFY `idacta_compromiso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idacta_compromiso` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `acta_responsabilidad`
 --
 ALTER TABLE `acta_responsabilidad`
-  MODIFY `idacta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idacta` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `ambiente`
 --
 ALTER TABLE `ambiente`
-  MODIFY `idambiente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idambiente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `articulo`
 --
 ALTER TABLE `articulo`
-  MODIFY `idarticulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idarticulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `idcategoria` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idcategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `equipo`
 --
 ALTER TABLE `equipo`
-  MODIFY `idequipo` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idequipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `novedad`
 --
 ALTER TABLE `novedad`
-  MODIFY `idnovedad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `idnovedad` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `programa`
 --
 ALTER TABLE `programa`
-  MODIFY `idprograma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idprograma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
--- vistas/modulos/inicio.php
+++ vistas/modulos/inicio.php
@@ -28,38 +28,6 @@
 
     </div>
 
-
-        
-              <div class="form-group">
-
-                <div class="input-group">
-
-                  <span class="input-group-addon">
-                    <img src="vistas/img/plantilla/modal/ambientes.png" width="15px">
-                  </span>
-
-                  <select class="form-control select2" style="width: 100%; border-radius: 0px">
-                    <option selected="selected">Seleccionar Ambiente</option>
-                      <?php
-
-                        $item  = null;
-                        $valor = null;
-
-                        $ambiente = ControladorAmbientes::ctrMostrarAmbientes($item, $valor);
-
-                        foreach ($ambiente as $key => $value) {
-
-                          echo '<option value="' . $value["idambiente"] . '">' . $value["nombreambiente"] . '</option>';
-                        }
-
-                      ?>
-                  </select>
-                
-                </div>
-
-              </div>
-
-
   </section>
 
 </div>
